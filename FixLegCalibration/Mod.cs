97bd5c4d35bd9ec76f5c60d98900bd93b85955f9
ZettaiVR <zettaivr@gmail.com>
2022-08-29 00:54:34 +0200

    Initial commit

---
+++ FixLegCalibration/Mod.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using RootMotion.FinalIK;
+using System;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Zettai.FixLegCalibration), "FixLegCalibration", "1.0", "Zettai")]
+[assembly: MelonGame(null, null)]
+
+namespace Zettai
+{
+    public class FixLegCalibration : MelonMod
+	{
+		[HarmonyPatch(typeof(VRIKCalibrator), nameof(VRIKCalibrator.CalibrateLeg), new Type[] { typeof(VRIKCalibrator.Settings),
+		typeof(Transform), typeof(IKSolverVR.Leg), typeof(Transform),typeof(Vector3), typeof(bool), typeof(float)})]
+		class CalibrateLegPatch
+		{
+            private const string LeftFootTarget = "Left Foot Target";
+            private const string RightFootTarget = "Right Foot Target";
+            private const string LeftLegBendGoal = "Left Leg Bend Goal";
+            private const string RightLegBendGoal = "Right Leg Bend Goal";
+
+            static bool Prefix(VRIKCalibrator.Settings settings, Transform tracker, IKSolverVR.Leg leg, Transform lastBone,
+				Vector3 rootForward, bool isLeft, float offset = 0f)
+			{
+				foreach (Transform obj in tracker.transform)
+					UnityEngine.Object.Destroy(obj.gameObject);
+
+				var footTargetName = isLeft ? LeftFootTarget : RightFootTarget;
+				var bendTargetName = isLeft ? LeftLegBendGoal : RightLegBendGoal;
+				var target = leg.target;
+				if (!target)
+					target = new GameObject(footTargetName).transform;
+				target.parent = lastBone;
+				target.localPosition = Vector3.zero;
+				target.localRotation = Quaternion.identity;
+				target.parent = tracker;
+				leg.target = target;
+				leg.positionWeight = 1f;
+				leg.rotationWeight = 1f;
+				var rotation = tracker.rotation * Quaternion.LookRotation(settings.footTrackerForward, settings.footTrackerUp);
+				var bendTarget = leg.bendGoal;
+				if (!bendTarget)
+					bendTarget = new GameObject(bendTargetName).transform;
+				bendTarget.parent = tracker;
+				bendTarget.position = lastBone.position + rotation * Vector3.forward + rotation * Vector3.up;
+				leg.bendGoal = bendTarget;
+				leg.bendGoalWeight = 1f;
+				return false;
+			}
+		}
+	}
+}
