52d4b9a2a0ab0fa804f3d03bc8e50886d21763d5
ZettaiVR <zettaivr@gmail.com>
2022-10-28 05:25:31 +0200

    Add FixDbLoadLag mod
    Update gitignore
    
    Add solution file

---
+++ FixDbLoadLag/Mod.cs
@@ -0,0 +1,53 @@
+﻿using ABI_RC.Core.Player;
+using HarmonyLib;
+using MelonLoader;
+using Zettai;
+
+[assembly: MelonInfo(typeof(FixDbLoadLag), "FixDbLoadLag", "1.0", "Zettai")]
+[assembly: MelonGame(null, null)]
+
+namespace Zettai
+{
+    public class FixDbLoadLag : MelonMod
+	{
+		private static MelonPreferences_Entry<bool> enableDbLagPatch;
+        public override void OnApplicationStart()
+		{
+			var category = MelonPreferences.CreateCategory("Zettai");
+			enableDbLagPatch = category.CreateEntry("enableDbLagPatch", true, "Enable Dynamic bone loading lag patch");
+		}
+        [HarmonyPatch(typeof(CVRDynamicBoneManager), nameof(CVRDynamicBoneManager.UpdateComponents))]
+        class UpdateDbComponentsPatch
+        {
+            public static bool letItRun = false;
+            static bool update = false;
+            static bool Prefix()
+            {
+                if (!enableDbLagPatch.Value)
+                    return true;
+                if (!letItRun)
+                {
+                    update = true;
+                    return false;
+                }
+                bool value = update;
+                update = false;
+                return value;
+            }
+        }
+        [HarmonyPatch(typeof(DbJobsColliderUpdate), nameof(DbJobsColliderUpdate.Update))]
+        class UpdateDbComponentsRun
+        {
+            static bool Prefix()
+            {
+                if (!enableDbLagPatch.Value)
+                    return true;
+                UpdateDbComponentsPatch.letItRun = true;
+                CVRDynamicBoneManager.UpdateComponents();
+                UpdateDbComponentsPatch.letItRun = false;
+                return true;
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-A ChilloutVR mod to fix leg calibration issues in full body tracking.
+ChilloutVR mods
 
 Not affiliated with or supported by ChilloutVR or Alpha Blend Interactive.
+
+FixDbLoadLag
+Fix the lag spike when loading avatars with dynamic bone components on it.
+
+FixLegCalibration
+Fix leg calibration issues in full body tracking. No longer needed as it's implemented in the game, kept here for reference.
+
