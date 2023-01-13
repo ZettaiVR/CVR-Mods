f91dddf8aef41a29a1837500fd3d996d8d177305
ZettaiVR <zettaivr@gmail.com>
2023-01-13 22:27:56 +0100

    Fix prop filter show and hide being reversed

--- MemoryCache/CacheItem.cs
+++ MemoryCache/CacheItem.cs
@@ -97,7 +97,7 @@ namespace Zettai
             if (hidden)
             {
                 MelonLoader.MelonLogger.Error($"Hidden by content filter: '{empty}', Asset ID: {assetId}, target: {target}, spawned by: {spawnedBy}, isVisible: {isVisible}, isOwnOrFriend: {isOwnOrFriend}, wasForceShown: {wasForceShown}, wasForceHidden: {wasForceHidden}', tags: {tags}.");
-                ABI_RC.Core.Util.AssetFiltering.AssetFilter.FilterProp(assetId, parent, tags.PropTags, isOwnOrFriend, isVisible, wasForceHidden, wasForceShown);
+                ABI_RC.Core.Util.AssetFiltering.AssetFilter.FilterProp(assetId, parent, tags.PropTags,isFriend: isOwnOrFriend, isVisible: isVisible, forceShow: wasForceShown, forceBlock: wasForceHidden);
                 yield break;
             }
             ClearTransformChildren(parent);
@@ -106,7 +106,7 @@ namespace Zettai
                 MelonLoader.MelonLogger.Msg($"OriginalItem: '{(OriginalItem != null ? OriginalItem.name : "-null-")}', instance: '{(instance != null ? instance.name : "-null-")}'.");
             yield return null;
             if (!mustShow)
-                ABI_RC.Core.Util.AssetFiltering.AssetFilter.FilterProp(assetId, instance, tags.PropTags, isOwnOrFriend, isVisible, wasForceHidden, wasForceShown);
+                ABI_RC.Core.Util.AssetFiltering.AssetFilter.FilterProp(assetId, parent, tags.PropTags, isFriend: isOwnOrFriend, isVisible: isVisible, forceShow: wasForceShown, forceBlock: wasForceHidden);
             else
                 MelonLoader.MelonLogger.Warning($"Prop force shown regardless of content filter. Asset ID: {assetId}, target: {target}, spawned by: {spawnedBy}, isVisible: {isVisible}, isOwnOrFriend: {isOwnOrFriend}, wasForceShown: {wasForceShown}, wasForceHidden: {wasForceHidden}', tags: {tags}.");
             if (!instance)
